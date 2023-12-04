80546e23c1c66abfb3d41eb1a43df09423b53dcc
Simana Poudel <poudelsimana22@gmail.com>
2023-12-04 08:59:19 +0545

    prisma added

--- app/(site)/component/AuthForm.tsx
+++ app/(site)/component/AuthForm.tsx
@@ -1,7 +1,10 @@
 "use client";
-import Input from '@/app/components/inputs/input';
+import Button from '@/app/components/Button';
+import Input from '@/app/components/inputs/Input';
 import {useCallback, useState} from 'react';
 import { useForm, FieldValues, SubmitHandler } from 'react-hook-form';
+import AuthSocialButton from './AuthSocialButton';
+import {BsGithub, BsGoogle} from 'react-icons/bs'
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -12,7 +15,7 @@ export const AuthForm = () => {
 
    const toggleVariant = useCallback(
     () => {
-      if (variant == 'LOGIN') {
+      if (variant === 'LOGIN') {
         setVariant("REGISTER");
       }
       else {
@@ -77,9 +80,109 @@ export const AuthForm = () => {
         className='space-y-6'
         onSubmit={handleSubmit(onSubmit)} //handleSubmit will pass the data to onSubmt
         >
-          <Input id='email' label='Email' register={register}/>
+          {variant === "REGISTER" && (
+            <Input 
+            id='name' 
+            label='Name' 
+            register={register} 
+            errors={errors}
+            disabled={isLoading}
+            />
+          )}
+          <Input 
+            id='email' 
+            label='Email Address'
+            type='email' 
+            register={register} 
+            errors={errors}
+            disabled={isLoading}
+            />
+            <Input 
+            id='password' 
+            label='Password'
+            type='password' 
+            register={register} 
+            errors={errors}
+            disabled={isLoading}
+            />
+            <div>
+              <Button 
+              disabled={isLoading}
+              fullWidth
+              type='submit'
+              >
+                {variant === 'LOGIN' ? 'Sign in' : 'Register' }
+              </Button>
+            </div>
         </form>
+        
+        <div className='mt-6'>
+          <div className='relative'>
+            <div 
+            className='
+            absolute
+            inset-0
+            flex
+            items-center
+            '>
+              <div 
+              className='
+              w-full
+              border-t
+              border-gray-300
+              '/>
+              </div>
+              <div 
+              className="
+              relative
+              flex
+              justify-center
+              text-sm
+              "
+              >
+              <span
+              className='
+              bg-white
+              text-gray-500
+              px-2
+              '>
+                Or continue with
+              </span>
+            </div>
+          </div>
 
+          <div className='mt-6 flex gap-2'>
+            <AuthSocialButton 
+            icon={BsGithub}
+            onClick={() => SocialAction('github')}
+            />
+            <AuthSocialButton 
+            icon={BsGoogle}
+            onClick={() => SocialAction('google')}
+            />
+          </div>
+        </div>
+        <div
+        className='
+        flex
+        mt-6
+        gap-2
+        justify-center
+        px-2
+        text-gray-500
+        text-sm
+        '>
+          <div>
+          {variant === 'LOGIN' ? "New To Messenger?" : "Already have an account?"}
+          </div>
+          <div
+          onClick={toggleVariant}
+          className='underline cursor-pointer'
+          >
+          {variant === 'LOGIN' ? "Create an account" : "Login"}
+            
+          </div>
+        </div>
       </div>
     </div>
   )
--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -55,9 +55,19 @@ const Input: React.FC<InputProps> = ({
         py-1.5
         text-gray-900
         shadow-sm
-        
+        ring-1
+        ring-inset
+        ring-gray-300
+        placeholder:text-gray-400
+        focus:ring-2
+        focus:ring-inset
+        focus:ring-sky-600
+        sm:text-sm
+        sm:leading-6 `,
+        errors[id] && "focus:ring-rose-500",
+        disabled && "opacity-50 cursor-default"
 
-        `)}
+        )}
         />
       </div>
     </div>
